32179438a5d297f0f866a84c2db29ea575137b9b
anand9773 <aaanand9773@gmail.com>
2024-01-27 22:30:36 +0530

    4Ddeveloper api project stracture is added

---
+++ src/main/java/com/javaApi/Ddeveloperapi/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.javaApi.Ddeveloperapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String listName;
+	private String email;
+
+}
